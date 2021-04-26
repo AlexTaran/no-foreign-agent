abedd2b67fabf04a6cbd6e34b609aa7388d1bae7
NobodyNoone <nobody@noone.com>
2021-04-26 11:49:41 +0200

    Added icons

--- extension/removeForeignAgent.js
+++ extension/removeForeignAgent.js
@@ -1,5 +1,10 @@
 
-var elements = document.querySelectorAll("div.Banner-root#div-gpt-ad");
-if (elements.length == 1) {
-  elements[0].remove();
-}
+var observer = new MutationObserver(function(mutations) {
+  var elements = document.querySelectorAll("div.Banner-root#div-gpt-ad");
+  if (elements.length == 1) {
+    elements[0].remove();
+  }
+});
+
+observer.observe(document, { childList: true, subtree: true });
+
